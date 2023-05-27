d0448824e9a9f4da834a42459c78d1bdda04865a
Unbekannt001 <77173218+Unbekannt001@users.noreply.github.com>
2023-05-27 23:19:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
+  ['4915257557001', 'D3adSoul', true],
   ['5492622271736'], 
   ['5492622271736'] 
 ] //Numeros de owner 
